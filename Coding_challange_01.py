5c2882eb8ed6c7cd13f04c18d5cf3bfae0e0f9e8
HazratMaghaz <maaz28608@gmail.com>
2025-08-28 23:21:10 +0500

    Coding challanges are the queries that may takes from the differernt sites and resources and i have try to solve it with what i have learned

---
+++ Coding_challange_01.py
@@ -0,0 +1,11 @@
+# Even and Odd Numbers
+
+number = int(input("Which number do you want to check? "))
+
+
+if number % 2 == 0:
+    print("Number is even")
+else:
+    print("Number is odd")
+
+    
