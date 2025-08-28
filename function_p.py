3bbe1c1eb7097794f4c7d6f70e952a5b9f10162d
HazratMaghaz <maaz28608@gmail.com>
2025-08-28 23:30:27 +0500

    I have solved sometechincal problesm from differernt resoucres there aand implrement what i have learned there , Functions strctures,

---
+++ function_p.py
@@ -0,0 +1,18 @@
+# def greeting(a):
+#     print("Hello world this would be the firt time am writing the code through the big screen and it seems like am writing on 300 X Zoom window . wow" , a)
+
+
+# hello = input("Please Enter your Name: ")
+# greeting(hello)    
+
+
+def main():
+    name = "Hazrat Maghaz"
+    Classs = "Master Level"
+    print(name, Classs , new())
+
+def new():
+    a =  input("Enter your name: ")
+    print(a)
+
+main()
