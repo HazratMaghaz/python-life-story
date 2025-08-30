ffe1cd79704259b27fce4f4eba59d6a228f983cb
HazratMaghaz <maaz28608@gmail.com>
2025-08-30 18:21:13 +0500

    New File : motif finder is added there

---
+++ motif_finder.py
@@ -0,0 +1,13 @@
+print("Welcome to the Motif Finder!")
+
+motif = ('ATCG', 'ATC','AGC')
+
+sequence_1 = input("Enter the DNA sequence you want to check motif's: ").upper()
+  
+          
+for m in sequence_1:
+    if m in sequence_1:
+        print(f"Motifs is present there")
+        break
+    else:
+        print("No Motifs founds")
